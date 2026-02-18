cdb56c91458742ecaca32e377e8b91008569a5b3
lksmeedia <lksmeedia@gmail.com>
2026-02-18 19:32:16 +0200

    lisasin koodi

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 375,
+        height: 812,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
